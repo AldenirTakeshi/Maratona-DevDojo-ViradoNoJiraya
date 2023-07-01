3dc2fd4490240108a01f71684424bdb76141381c
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-07-01 12:36:53 -0400

    VarArgs

--- untitled/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/ImpressoraEstudante.java
+++ untitled/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/ImpressoraEstudante.java
@@ -5,6 +5,7 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudant
 public class ImpressoraEstudante {
     public  void imprime(Estudante estudante) {
 
+
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
