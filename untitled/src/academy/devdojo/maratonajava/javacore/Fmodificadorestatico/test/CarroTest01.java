b07f20fce1fccc20eaca2d6cb25ffc0212dad00a
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-07-02 18:41:24 -0400

    modificadores static

---
+++ untitled/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro01 = new Carro("BMW", 280);
+        Carro carro02 = new Carro("Mercedes", 275);
+        Carro carro03 = new Carro("AUDI", 290);
+
+        Carro.velocidadeLimite = 180;
+
+        carro01.imprime();
+        carro02.imprime();
+        carro03.imprime();
+
+    }
+}
