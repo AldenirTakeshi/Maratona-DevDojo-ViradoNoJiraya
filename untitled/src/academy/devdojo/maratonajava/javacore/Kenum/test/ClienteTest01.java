3492b7ba2faed96f439e76f1ea1db8c3a77b03c8
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-07-09 07:38:15 -0400

    enum sobrescrita de metodos

--- untitled/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ untitled/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -2,16 +2,16 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.domain.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente01 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente02 = new Cliente("Tsubasa",TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente03 = new Cliente("Tsubasa",TipoCliente.PESSOA_FISICA);
-        Cliente cliente04 = new Cliente("Tsubasa",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente01 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente02 = new Cliente("Akira",TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
         System.out.println(cliente01);
         System.out.println(cliente02);
-        System.out.println(cliente03);
-        System.out.println(cliente04);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
