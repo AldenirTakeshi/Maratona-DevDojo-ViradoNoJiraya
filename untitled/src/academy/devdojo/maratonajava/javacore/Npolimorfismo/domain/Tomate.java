a0ddc1356015388bcfbfcfb81b5afcf91de117c0
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-07-11 12:42:23 -0400

    polimorfismo04

--- untitled/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/domain/Tomate.java
+++ untitled/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/domain/Tomate.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.domain;
 
 public class Tomate extends Produto{
+    private String dataValidade;
     public static final double IMPOSTO_POR_CENTO = 0.06;
 
     public Tomate(String nome, double valor) {
@@ -13,4 +14,11 @@ public class Tomate extends Produto{
         return this.valor * IMPOSTO_POR_CENTO;
     }
 
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
 }
